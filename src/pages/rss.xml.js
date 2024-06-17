6bf1451c51673b32cd4e52024f04200dc8121f57
wuhaowen <wuhw@e-fts.cn>
2024-06-17 17:46:03 +0800

    rss修改

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,15 +1,16 @@
 import rss from '@astrojs/rss';
-import { getCollection } from 'astro:content';
 import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
+import { getSortedPosts } from '../utils/content-utils';
 
 export async function GET(context) {
-	const posts = await getCollection('posts');
+	const posts = await getSortedPosts();
 	return rss({
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
 		site: context.site,
 		items: posts.map((post) => ({
 			...post.data,
+			pubDate: post.data,
 			link: `/posts/${post.slug}/`,
 		})),
 	});
